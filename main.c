58b1e07203596643b40fe2ab16017a85deaefb45
Davi Lorena <davilorena2016@gmail.com>
2026-04-16 12:07:45 -0300

    feat: adiciona arquivos do projeto

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "ordenacao.h"
+
+typedef struct metricas {
+unsigned long long comp, moviment;
+} met;
+
+typedef struct request {
+int id, x;
+} r;
+
+
+int main() {
+
+    // Inicia a contagem de tempo 
+
+    
+        // Chama o algoritmo de ordenação 
+
+        
+    // Finaliza a contagem de tempo 
+
+    return 0;
+
+}
